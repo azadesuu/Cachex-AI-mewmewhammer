2e024d0b6b8610ce95b55dd35f2fd49129e5b067
Celene Saw <azadesu.github@gmail.com>
2022-03-31 23:16:58 +1100

    adding comments

--- search/main.py
+++ search/main.py
@@ -1,9 +1,19 @@
 import sys
 import json
-# from search.util import valid_adjacent_nodes
 # If you want to separate your code into separate files, put them
 # inside the `search` directory (like this one and `util.py`) and
 # then import from them like this:
+
+"""
+COMP30024 Artificial Intelligence, Semester 1, 2022
+Project Part A: Searching
+Authored by: Ying Shan Saw & Jo Eann Chong
+Date Authored: 31/03/2022
+
+This script contains the entry point to the program (the code in
+`__main__.py` calls `main()`). Your solution starts here!
+"""
+
 from util import pathfinding
 from classes import Board
 
