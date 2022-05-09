7d5c3aedc9221d042eda5c53393926779a424d5e
Celene Saw <azadesu.github@gmail.com>
2022-05-09 18:59:20 +1000

    push code

--- skeleton-code-B/mewmewhammer/util.py
+++ skeleton-code-B/mewmewhammer/util.py
@@ -3,81 +3,111 @@ import random
 from numpy import zeros
 
 def scoring_system(player):
-        board = player.board
-        ### initialising##
-        n = board.n
-        # board empty
-        if board._data == zeros((n, n), dtype=int):
-            # choose maximum value
-            # sets the value of each placement
-            board_score = {(v,k):0 for (v,k) in range(0, n)}
-            for i in range(0, n):
-                for j in range (0,n):
-                    if (i==0) and (j==5):
-                        # cannot be swapped
-                        board_score = 10
-                    if (i == j):    
-                        board_score[(i,j)] = 5
-                        # calculate distance
-                    elif (i == 0) or (j==0):
-                        board_score[(i,j)] = 5
-                    else:
-                        pass
-                        #calculate distance
-            
-            if (player.player == "red") and (n%2==1):
-                board_score[(n/2,n/2)] = -100
-            elif (player.player == "red"):
-                board_score[(player.board.n, player.board.n)] = 100
-            elif (player.player == "blue"):
-                red_coord = player.last_coord
-                if (red_coord[0]!= red_coord[1]  and player.count == 1):
-                    board_score[(red_coord[1],red_coord[0])] = "STEAL"
-            else:
-                #center pieces
-                board_score[(n/2,n/2)] = 10
-        # done initialising base scores   
+    # efficient
+    # estimating utility
+    # estimate who is going to win
+    """
+    board = player.board
+    ### initialising##
+    n = board.n
+    # board empty
+    if board._data == zeros((n, n), dtype=int):
+        # choose maximum value
+        # sets the value of each placement
+        board_score = {(v,k):0 for (v,k) in range(0, n)}
+        for i in range(0, n):
+            for j in range (0,n):
+                if (i==0) and (j==5):
+                    # cannot be swapped
+                    board_score = 10
+                if (i == j):    
+                    board_score[(i,j)] = 5
+                    # calculate distance
+                elif (i == 0) or (j==0):
+                    board_score[(i,j)] = 5
+                else:
+                    pass
+                    #calculate distance
         
-        ## checking for blue's move, if threatens capture or long
-        if (player.enemy_last_coord != (-1,-1)):
-            threaten_capture = can_capture(player.enemy_last_coord, player, "defense")
-            ## defend, update score board to prioritise defense
-            enemy_list = player.board.connected_coords(player.enemy_last_coord)
-            index_list = list()
-            for node in enemy_list:
-                index_list.append(node[player.player_goal])
-            if (max(index_list) - min(index_list)) <= math.floor(n/2):
-                pass
-                #threaten capture middle if possible, else just threaten capture the ends
-                #check if any nodes capturable using function
+        if (player.player == "red") and (n%2==1):
+            board_score[(n/2,n/2)] = -100
+        elif (player.player == "red"):
+            board_score[(player.board.n, player.board.n)] = 100
+        elif (player.player == "blue"):
+            red_coord = player.last_coord
+            if (red_coord[0]!= red_coord[1]  and player.count == 1):
+                board_score[(red_coord[1],red_coord[0])] = "STEAL"
+        else:
+            #center pieces
+            board_score[(n/2,n/2)] = 10
+    # done initialising base scores   
+    """
 
-
-        ## update goal path (done in player)
-            ## add points
-        ## check the top valid locations for capture
-
-
-
+    ## checking for blue's move, if threatens capture or long
+    if (player.enemy_last_coord != (-1,-1)):
+        threaten_capture = can_capture(player.enemy_last_coord, player, "defense")
         
-            
+        
+        ## defend, update score board to prioritise defense
+        enemy_list = player.board.connected_coords(player.enemy_last_coord)
+        index_list = list(); index_list.append([]); index_list.append([])
+        for node in enemy_list:
+            index_list[0].append(node[player.player_goal])
+            index_list[0].append(node[1-player.player_goal])
+
+        if (max(index_list) - min(index_list)) <= math.floor(n/2):
+            pass
+            #threaten capture middle if possible, else just threaten capture the ends
+            #check if any nodes capturable using function
+        elif (max(index_list.count(index_list[0]))==n-1):
+            # erecting barrier
+            # looks for the empty spot in the barricade
+            coord_list = list(set([i in range(0,n)])^set(index_list))
+            coordinate = []; 
+            coordinate[0]
+            board_score[tuple(coordinate)]
 
 
 
-        ## checking for next step in goal path
+    ## update goal path (done in player)
+        ## add points
+    ## check the top valid locations for capture
+
+    ### utility function (MAX)
+    # red vs blue pieces (red-blue) (ONLY THIS)
+    # distance to the either side
+    # length of goal path (positive)
+
+    
+    # transposition table for all moves (branching factor is big)
+    
+    # blue captured = (+ score)
+    # red captured = (negative -score)
+    # 
 
 
 
-        # red will always start from 5,5
-        # blue will always start from 0,n n,n n,0 n,n unless can capture
+
+    
+        
 
 
-        valid_locations = player.board.get_valid_locations()
-            
 
-        # check if previous move threatens capture
-        # else continue greedy algorithm
+    ## checking for next step in goal path
 
-        return []
+
+
+    # red will always start from 5,5
+    # blue will always start from 0,n n,n n,0 n,n unless can capture
+
+
+    valid_locations = player.board.get_valid_locations()
+        
+
+    # check if previous move threatens capture
+    # else continue greedy algorithm
+
+    return []
 # checks for nodes adjacent to the current node, and returns the valid ones
 def valid_adjacent_nodes(current:tuple, player):
     board = player.board
@@ -117,49 +147,56 @@ def generated_adj_nodes(current: tuple):
                 node.append(current[1] + y)
                 adj_nodes.append(tuple(node)) # converting to tuple to use as dictionary key
     return adj_nodes
-def can_capture(node_to_place, player, mode):
-    # if mode == defense, node_to_place will be red's (aim to return false)
-    # if mode = offense, node_to_place will be blue's (aim to return true)
+
+def can_capture(node_to_place, player):
     board = player.board
+
+    # which enemy pieces get captured
+    captured_boolean = False
+    captured = list()
+
+    return captured_boolean, [captured]
+
+
 
 def is_terminal_node(player):
 	return len(player.board.get_valid_locations()) == 0        
 
 def minimax(player, depth, alpha, beta):
-        valid_locations = player.board.get_valid_locations
-        is_terminal = is_terminal_node(player)
-        if depth == 0 or is_terminal:
-            if is_terminal:
-                return (None, 0)
-            # depth = 0
-            return (None, scoring_system(player))
-    
-        # maximising player
-        if player == "red":
-            value = -math.inf
-            column = random.choice(valid_locations)
-            for col in valid_locations:
-                new_score = minimax(b_copy, depth-1, alpha, beta, False)[1]
-                if new_score > value:
-                    value = new_score
-                    column = col
-                alpha = max(alpha, value)
-                if alpha >= beta:
-                    break
-            return column, value
+    valid_locations = player.board.get_valid_locations
+    is_terminal = is_terminal_node(player)
+    if depth == 0 or is_terminal:
+        if is_terminal:
+            return (None, 0)
+        # depth = 0
+        return (None, scoring_system(player))
 
-        else: # Minimizing player
-            value = math.inf
-            column = random.choice(valid_locations)
-            for col in valid_locations:
-                row = get_next_open_row(board, col)
-                b_copy = board.copy()
-                drop_piece(b_copy, row, col, PLAYER_PIECE)
-                new_score = minimax(b_copy, depth-1, alpha, beta, True)[1]
-                if new_score < value:
-                    value = new_score
-                    column = col
-                beta = min(beta, value)
-                if alpha >= beta:
-                    break
-            return column, value
+    # maximising player
+    if player == "red":
+        value = -math.inf
+        column = random.choice(valid_locations)
+        for col in valid_locations:
+            new_score = minimax(b_copy, depth-1, alpha, beta, False)[1]
+            if new_score > value:
+                value = new_score
+                column = col
+            alpha = max(alpha, value)
+            if alpha >= beta:
+                break
+        return column, value
+
+    else: # Minimizing player
+        value = math.inf
+        column = random.choice(valid_locations)
+        for col in valid_locations:
+            row = get_next_open_row(board, col)
+            b_copy = board.copy()
+            drop_piece(b_copy, row, col, PLAYER_PIECE)
+            new_score = minimax(b_copy, depth-1, alpha, beta, True)[1]
+            if new_score < value:
+                value = new_score
+                column = col
+            beta = min(beta, value)
+            if alpha >= beta:
+                break
+        return column, value
